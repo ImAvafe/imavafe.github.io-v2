cb4d950349e29be26f67f631814c14a50f8a9593
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-07-05 16:09:13 -0400

    Add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import daisyui from "daisyui"
+import themes from "daisyui/src/theming/themes"
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -9,5 +10,26 @@ export default {
   plugins: [
     daisyui,
   ],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...themes["dark"],
+          primary: "#9F84FF",
+          secondary: "#212121",
+          neutral: "#212121",
+          "base-100": "#0E0E0E",
+        },
+        light: {
+          ...themes["light"],
+          primary: "#9F84FF",
+          secondary: "#212121",
+          neutral: "#212121",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
 }
 
