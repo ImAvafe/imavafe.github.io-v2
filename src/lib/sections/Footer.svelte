b5a62b8c67d3b02dcfca46cf318cb200ada35c64
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-07-09 16:54:59 -0400

    Add footer

---
+++ src/lib/sections/Footer.svelte
@@ -0,0 +1,9 @@
+<div class="pt-24 pb-12">
+  <center>
+    Made with Svelte, Tailwind and DaisyUI
+    <br>
+    Source code available on <a href="https://github.com/imavafe/imavafe.github.io" target="_blank" rel="noopener noreferrer" class="underline">Github</a>
+    <br>
+    ✌️
+  </center>
+</div>
