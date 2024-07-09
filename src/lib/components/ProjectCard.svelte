82895a45aed656ab4735351fc2cc08056bf95f06
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-07-09 17:40:36 -0400

    Use platform as icon alt

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -9,7 +9,7 @@
   <div class="relative block w-full aspect-video rounded-xl ring-white/20 ring-1 overflow-hidden transition duration-300 hover:duration-300 hover:ring-primary hover:ring-2 hover:bg-fixed">
     <img src={project.bannerSrc} alt="Project thumbnail" class="w-full h-full duration-500 hover:duration-500 hover:scale-105" />
     <span class="absolute bottom-0 left-0 right-0 bg-black/60 p-2 pl-4 pr-4 text-2xl text-white font-medium flex items-center justify-between">
-      <p class="text-xl">{project.displayName}</p><img src="/src/lib/icons/{project.platform.toLowerCase()}.svg" alt="Site icon" class="h-8" />
+      <p class="text-xl">{project.displayName}</p><img src="/src/lib/icons/{project.platform.toLowerCase()}.svg" alt="{project.platform}" class="h-8" />
     </span>
   </div>
 </a>
