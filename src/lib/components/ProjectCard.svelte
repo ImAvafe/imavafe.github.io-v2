51c5fe50f63ecfc98392c08020f01c434505531c
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-07-09 16:55:13 -0400

    Add noopener norefferer  to ProjectCard

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -5,7 +5,7 @@
   export let bannerSrc = "/src/lib/img/projects/Poser.png"
 </script>
 
-<a href={href} target="_blank">
+<a href={href} target="_blank" rel="noopener noreferrer">
   <div class="relative block w-full aspect-video rounded-xl ring-white/20 ring-1 overflow-hidden transition duration-300 hover:duration-300 hover:ring-primary hover:ring-2 hover:bg-fixed">
     <img src={bannerSrc} alt="Project thumbnail" class="w-full h-full duration-500 hover:duration-500 hover:scale-105" />
     <span class="absolute bottom-0 left-0 right-0 bg-black/60 p-2 pl-4 pr-4 text-2xl text-white font-medium flex items-center justify-between">
