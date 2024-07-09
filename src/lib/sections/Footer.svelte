54cdcfe2fd1f3ae206b6ff7addbd7ec184c76459
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-07-09 18:49:25 -0400

    Add message

--- src/lib/sections/Footer.svelte
+++ src/lib/sections/Footer.svelte
@@ -1,9 +1,11 @@
 <div class="pt-24 pb-12">
   <center>
-    Made with Svelte, Tailwind and DaisyUI
+    Made with TS, Svelte, Tailwind and DaisyUI
     <br>
     Source code available on <a href="https://github.com/imavafe/imavafe.github.io" target="_blank" rel="noopener noreferrer" class="underline">Github</a>
     <br>
     ✌️
+
+    <p class="hidden select-none">secret.athml@simplelogin.com<br>wow, what a gunter 🥚<br>so what do you think of my code?</p>
   </center>
 </div>
