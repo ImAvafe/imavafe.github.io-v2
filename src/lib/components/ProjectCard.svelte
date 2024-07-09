71e2379563a609207824c22987dc3737e8e42129
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-07-09 13:24:07 -0400

    Fix ProjectCard thumbnail loading

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -7,7 +7,7 @@
 
 <a href={href} target="_blank">
   <div class="relative block w-full aspect-video rounded-xl ring-white/20 ring-1 overflow-hidden transition duration-300 hover:duration-300 hover:ring-primary hover:ring-2 hover:bg-fixed">
-    <div class="bg-[url('{bannerSrc}')] bg-cover w-full h-full duration-500 hover:duration-500 hover:scale-105"></div>
+    <img src={bannerSrc} alt="Project thumbnail" class="w-full h-full duration-500 hover:duration-500 hover:scale-105" />
     <span class="absolute bottom-0 left-0 right-0 bg-black/60 p-2 pl-4 pr-4 text-2xl text-white font-medium flex items-center justify-between">
       <p class="text-xl">{name}</p><img src="{iconSrc}" alt="Site icon" class="h-8" />
     </span>
--- src/lib/sections/Projects.svelte
+++ src/lib/sections/Projects.svelte
@@ -2,6 +2,7 @@
   import ProjectCard from "$lib/components/ProjectCard.svelte";
 </script>
 
-<div class="w-full">
-  <ProjectCard name="Poser! 🤔" iconSrc="/src/lib/icons/roblox.svg" href="https://www.roblox.com/games/12716101148" bannerSrc="/src/lib/img/projects/Poser.png" />
-</div>
+<span class="w-full flex-col gap-3 inline-flex">
+  <ProjectCard name="Poser! 🤔" iconSrc="/src/lib/icons/roblox.svg" href="https://www.roblox.com/games/12716101148" bannerSrc="/src/lib/img/projects/poser.png" />
+  <ProjectCard name="OnyxUI" iconSrc="/src/lib/icons/github.svg" href="https://www.roblox.com/games/12716101148" bannerSrc="/src/lib/img/projects/OnyxUI.png" />
+</span>
