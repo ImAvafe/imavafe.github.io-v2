3e993daad0b8cab154eb9fe14aaed415513f8fbe
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-07-10 15:14:51 -0400

    Add hover effect to SocialLink

--- src/lib/components/SocialLink.svelte
+++ src/lib/components/SocialLink.svelte
@@ -1,7 +1,18 @@
 <script>
   export let href = "https://example.com";
-  export let src = "/src/lib/icons/roblox.svg";
-  export let alt = "Social link";
 </script>
 
-<a role="button" href={href} target="_blank" rel="noopener noreferrer" class="btn btn-link btn-square btn-lg"><img src={src} alt={alt} /></a>
+<a role="button" href={href} target="_blank" rel="noopener noreferrer" class="btn btn-link btn-square btn-lg">
+  <slot />
+</a>
+
+<style>
+  a :global(svg path) {
+    transition-duration: 300ms;
+  }
+
+  a:hover :global(svg path) {
+    fill: #9F84FF;
+    transition-duration: 300ms;
+  }
+</style>
