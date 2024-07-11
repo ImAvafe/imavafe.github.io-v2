3f724d2d421be10c7b2350b90493c207f2530535
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-07-11 12:55:28 -0400

    Add pulsing code background effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import Projects from "$lib/sections/Projects.svelte";
 </script>
 
-<div class="w-full pt-14 flex-col justify-center items-center inline-flex">
+<div class="pt-14 flex-col justify-center items-center inline-flex">
   <div class="max-w-5xl w-96 flex-col justify-center items-center inline-flex">
     <Intro />
     <div class="divider pl-10 pr-10"></div>
