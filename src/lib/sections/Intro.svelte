29de534ec645623f1489a629e70f2ceaea95678f
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-07-13 15:55:36 -0400

    The PFP will definitely work this time..

--- src/lib/sections/Intro.svelte
+++ src/lib/sections/Intro.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
   import Logo from "$lib/components/Logo.svelte";
   import SocialLink from "$lib/components/SocialLink.svelte";
-  import ProfilePicture from "$lib/img/ProfilePicture.png"
+  import PFP from "$lib/img/PFP.png";
 </script>
 
 <div class="flex-col justify-start items-center gap-4 inline-flex">
   <div class="avatar">
     <div class="w-44 rounded-full">
-      <img src={ProfilePicture} alt="Avafe's PFP"/>
+      <img src={PFP} alt="Avafe's PFP"/>
     </div>
   </div>
   <div class="w-64 h-24 flex-col justify-start items-center gap-2.5 inline-flex">
