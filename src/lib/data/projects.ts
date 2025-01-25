f2a141b131f9aec97f16a98a8b897769ab9a134e
Avafe <65048459+ImAvafe@users.noreply.github.com>
2025-01-25 16:45:45 -0500

    Add SayHey2Me

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -24,6 +24,7 @@ import RoroomsBanner from '$lib/img/projects/Rorooms.png';
 import LonekaBanner from '$lib/img/projects/Loneka.png';
 import AvalogBanner from '$lib/img/projects/Avalog.png';
 import MoonletsBanner from '$lib/img/projects/Moonlets.png';
+import SayHey2MeBanner from '$lib/img/projects/SayHey2Me.png';
 
 export const projects: Project[] = [
 	{
@@ -90,13 +91,13 @@ export const projects: Project[] = [
 		bannerSrc: neoHotbarBanner
 	},
 	{
-		id: 'ClimbOfChaos',
-		displayName: 'Climb Of Chaos',
-		type: 'game',
-		platform: 'roblox',
-		href: 'https://www.roblox.com/games/16882394262',
-		creationStamp: 1711425600,
-		bannerSrc: climbOfChaosBanner
+		id: 'Moonlets',
+		displayName: 'Moonlets',
+		type: 'resource',
+		platform: 'github',
+		href: 'https://github.com/imavafe/moonlets',
+		creationStamp: 1710993600,
+		bannerSrc: MoonletsBanner
 	},
 	{
 		id: 'KittyDash',
@@ -108,16 +109,6 @@ export const projects: Project[] = [
 		bannerSrc: kittyDashBanner
 	},
 	{
-		id: 'Moonlets',
-		displayName: 'Moonlets',
-		type: 'resource',
-		platform: 'github',
-		href: 'https://github.com/imavafe/moonlets',
-		creationStamp: 1710993600,
-		bannerSrc: MoonletsBanner
-	},
-
-	{
 		id: 'AvafeMev2',
 		displayName: 'avafe.me v2',
 		type: 'other',
@@ -125,6 +116,24 @@ export const projects: Project[] = [
 		href: 'https://github.com/ImAvafe/imavafe.github.io',
 		creationStamp: 1720152000,
 		bannerSrc: avafeMeBanner
+	},
+	{
+		id: 'SayHey2Me',
+		displayName: 'SAYHEY2ME',
+		type: 'other',
+		platform: 'website',
+		href: 'https://sayhey2.me',
+		creationStamp: 1735534800,
+		bannerSrc: SayHey2MeBanner
+	},
+	{
+		id: 'ClimbOfChaos',
+		displayName: 'Climb Of Chaos',
+		type: 'game',
+		platform: 'roblox',
+		href: 'https://www.roblox.com/games/16882394262',
+		creationStamp: 1711425600,
+		bannerSrc: climbOfChaosBanner
 	},
 	{
 		id: 'Devcember2023',
