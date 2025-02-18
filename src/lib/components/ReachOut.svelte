bad378cec6e1c638342912a36262947aba82fb48
Avafe <65048459+ImAvafe@users.noreply.github.com>
2025-02-17 20:06:26 -0500

    Add chat call to action

---
+++ src/lib/components/ReachOut.svelte
@@ -0,0 +1,45 @@
+<div class="my-16 flex flex-col items-center gap-14">
+  <h2 class="font-bold text-3xl text-center">
+    Something pique your interest..?
+  </h2>
+  <span class="cl-effect-1">
+    <a href="/discord-dms" target="_blank" rel="noopener noreferrer" class="text-primary text-2xl">Let's chat</a>
+  </span>
+</div>
+
+<style>
+  .cl-effect-1 a::before,
+  .cl-effect-1 a::after {
+    display: inline-block;
+    opacity: 0;
+    -webkit-transition: -webkit-transform 0.3s, opacity 0.2s;
+    -moz-transition: -moz-transform 0.3s, opacity 0.2s;
+    transition: transform 0.3s, opacity 0.2s;
+  }
+
+  .cl-effect-1 a::before {
+    margin-right: 10px;
+    content: '[';
+    -webkit-transform: translateX(20px);
+    -moz-transform: translateX(20px);
+    transform: translateX(20px);
+  }
+
+  .cl-effect-1 a::after {
+    margin-left: 10px;
+    content: ']';
+    -webkit-transform: translateX(-20px);
+    -moz-transform: translateX(-20px);
+    transform: translateX(-20px);
+  }
+
+  .cl-effect-1 a:hover::before,
+  .cl-effect-1 a:hover::after,
+  .cl-effect-1 a:focus::before,
+  .cl-effect-1 a:focus::after {
+    opacity: 1;
+    -webkit-transform: translateX(0px);
+    -moz-transform: translateX(0px);
+    transform: translateX(0px);
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
 <script>
   import Intro from "$lib/sections/Intro.svelte";
 	import Projects from "$lib/sections/Projects.svelte";
+  import ReachOut from "$lib/components/ReachOut.svelte";
 </script>
 
 <div class="pt-14 flex-col justify-center items-center inline-flex">
-  <div class="max-w-6xl w-96 flex-col justify-center items-center inline-flex px-4">
+  <div class="max-w-6xl gap-4 w-96 flex-col justify-center items-center inline-flex px-4">
     <Intro />
     <div class="divider pl-10 pr-10"></div>
     <Projects />
+    <div class="divider pl-10 pr-10"></div>
+    <ReachOut />
   </div>
 </div>
