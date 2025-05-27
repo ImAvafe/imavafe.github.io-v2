ff5af30f4c144723a03a7e9994686c8db5dacd2a
Avafe <65048459+ImAvafe@users.noreply.github.com>
2025-05-26 20:27:12 -0400

    Write about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,20 @@
 <script>
-  import ReachOut from "$lib/components/ReachOut.svelte";
-import Intro from "$lib/sections/Intro.svelte";
+  import Intro from "$lib/sections/Intro.svelte";
+  import { openDiscordDms } from "$lib/openDiscordDms";
 </script>
 
 <span class="flex flex-col gap-16 justify-center max-w-md">
   <Intro />
   <article class="prose">
     <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet consectetur. Varius risus adipiscing scelerisque scelerisque. Ultrices mauris varius neque scelerisque. Suscipit etiam ultricies at pharetra sed mauris donec.</p>
-    <p>Adipiscing nascetur elit faucibus dolor aenean faucibus bibendum. Varius praesent consectetur egestas semper quisque nisl quis egestas pellentesque.</p>
-    <p>Donec a mi euismod leo maecenas rhoncus in proin et. Lorem vulputate enim porta curabitur id sit aenean vitae tincidunt.</p>
+    <p>I'm a games & software developer, primarily honed in on programming and design. Much of my professional work is done on Roblox, though I also enjoy playing with web and app technologies.</p>
+    <p>My work is primarily housed under my solo studio, Loneka. Its <a href="https://loneka.com">games</a> have accrued over 40M sessions, and its <a href="https://loneka.com/suite">open-source software</a> will be soon utilized by billions more.</p>
+    <p>I was first exposed to tech in the family barn, seeing a variety of things early on: terminal scripts, websites, Linux, circuity, and good old heavy machinery. 🚜🤠</p>
+    <p>Fast-forward several years, I'd fallen in love with videogames, and I was suddenly in need of money. Nowadays, work is much more a willing passion for me, and I feel excited to sit down most days and make things.</p>
+    <p>I've also recently gone down the academic pipeline, and many of my work hours now go to preparing for university.</p>
+    <p>When I'm not working or studying, my hobbies include: hanging out with others, thinking, reading <i>(audiobooks 💀)</i>, and of course, playing games.</p>
+    <br />
+
     <h1>Tools</h1>
     <span class="flex flex-col items-center gap-2">
       <img class="m-0" src="https://go-skill-icons.vercel.app/api/icons?i=luau,ts,html,css" alt="Languages">
@@ -17,5 +22,15 @@ import Intro from "$lib/sections/Intro.svelte";
       <img class="m-0" src="https://go-skill-icons.vercel.app/api/icons?i=vscode,figma,github,blender" alt="Tools">
       <img class="m-0" src="https://go-skill-icons.vercel.app/api/icons?i=windows,brave,obsidian,signal,discord" alt="Personal Tools">
     </span>
+    <br />
+
+    <h1>Contact</h1>
+    <p>
+      I mainly message through <a on:click={openDiscordDms} role="button">Discord</a>, 
+      but you can also email me at <a href="mailto:ava@avafe.me">ava@avafe.me</a>.
+      Use whichever you prefer, as I'm indifferent.
+    </p>
+    <p><b>Let's connect! ☝️🤓</b> <i>(this is where I would put my LinkedIn if they didn't ban me for reasons unbeknownst to me)</i></p>
+    <br />
   </article>
 </span>
