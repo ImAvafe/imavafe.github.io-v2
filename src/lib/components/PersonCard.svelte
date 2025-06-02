4529cb757201db512209ec8e947842ed1ef5b0ef
Avafe <65048459+ImAvafe@users.noreply.github.com>
2025-06-02 18:14:24 -0400

    Add vouch page

---
+++ src/lib/components/PersonCard.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let name = "John Doe";
+  export let message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  export let image = "https://cdn.mos.cms.futurecdn.net/THCiUmVZcgxHodGCK3EyYo.jpg";
+  export let href = "https://example.com";
+</script>
+
+<button class="btn max-w-sm h-auto p-0 overflow-hidden border-2 border-white/5">
+  <a href={href} target="_blank" rel="noopener noreferrer" class="flex flex-row gap-2 overflow-hidden text-left font-medium">
+    <img src={image} alt="Face of {name}" class="h-full w-24">
+    <div class="flex flex-col items-start p-2">
+      <h1 class="font-bold text-lg">{name}</h1>
+      <p class="text-pretty">{message}</p>
+    </div>
+  </a>
+</button>
---
+++ src/lib/data/vouches.ts
@@ -0,0 +1,47 @@
+export type Vouch = {
+	name: string;
+	message?: string;
+	image: string;
+	href: string;
+};
+
+export const vouches: Vouch[] = [];
+
+// export const vouches: Vouch[] = [
+// 	{
+// 		name: 'Jane Doe',
+// 		message: 'This service has been a game changer for my business!',
+// 		image: 'https://cdn.mos.cms.futurecdn.net/THCiUmVZcgxHodGCK3EyYo.jpg',
+// 		href: 'https://example.com/'
+// 	},
+// 	{
+// 		name: 'Jane Doe',
+// 		message: 'I was impressed by the professionalism and quick turnaround.',
+// 		image: 'https://cdn.mos.cms.futurecdn.net/THCiUmVZcgxHodGCK3EyYo.jpg',
+// 		href: 'https://example.com/'
+// 	},
+// 	{
+// 		name: 'Jane Doe',
+// 		message: 'Excellent communication and top-notch results every time.',
+// 		image: 'https://cdn.mos.cms.futurecdn.net/THCiUmVZcgxHodGCK3EyYo.jpg',
+// 		href: 'https://example.com/'
+// 	},
+// 	{
+// 		name: 'Jane Doe',
+// 		message: 'I highly recommend this to anyone looking for quality work.',
+// 		image: 'https://cdn.mos.cms.futurecdn.net/THCiUmVZcgxHodGCK3EyYo.jpg',
+// 		href: 'https://example.com/'
+// 	},
+// 	{
+// 		name: 'Jane Doe',
+// 		message: 'The attention to detail and dedication exceeded my expectations.',
+// 		image: 'https://cdn.mos.cms.futurecdn.net/THCiUmVZcgxHodGCK3EyYo.jpg',
+// 		href: 'https://example.com/'
+// 	},
+// 	{
+// 		name: 'Jane Doe',
+// 		message: 'A seamless experience from start to finish—thank you!',
+// 		image: 'https://cdn.mos.cms.futurecdn.net/THCiUmVZcgxHodGCK3EyYo.jpg',
+// 		href: 'https://example.com/'
+// 	}
+// ];
